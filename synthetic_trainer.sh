329903498065df8bc217b03af0a526342382014d
Nazanin AlipourFard <nazaninalipourfard@Nazanins-MacBook-Pro.local>
2019-12-13 22:05:24 -0800

    going to change decoder

--- synthetic_trainer.sh
+++ synthetic_trainer.sh
@@ -1,4 +1,4 @@
-python mixhop_trainer.py --dataset_name=sim_train --adj_pows=0:20:6,1:20:6,2:20:6 --hidden_dims_csv=60,18 \
+python mixhop_trainer.py --dataset_name=train --adj_pows=0:20:6,1:20:6,2:20:6 --hidden_dims_csv=60,18 \
   --learn_rate=0.1 --lr_decrement_every=40 --early_stop_steps=200 \
   --input_dropout=0.5 --layer_dropout=0.9 --l2reg=5e-2 \
   --retrain
